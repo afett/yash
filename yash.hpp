5ed69d36c45fb0647e8667d2c4597f87e2d3e3ae
Andreas Fett <a.fett@gmx.de>
2015-05-06 20:50:12 +0200

    update copyright

--- yash.hpp
+++ yash.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014 Andreas Fett.
+ * Copyright (c) 2014, 2015 Andreas Fett.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
